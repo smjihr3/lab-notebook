75a4c52cc599672b08c8a114871d0ca49f5b1c31
smjihr3 <smjihr3@gmail.com>
2026-03-15 20:32:51 +0900

    feat: GraphPage에 GraphView 연결

--- src/pages/GraphPage.jsx
+++ src/pages/GraphPage.jsx
@@ -1,7 +1,9 @@
+import GraphView from '../features/graph/GraphView'
+
 export default function GraphPage() {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold text-gray-800">연결 그래프</h1>
+    <div className="h-full w-full">
+      <GraphView />
     </div>
   )
 }
